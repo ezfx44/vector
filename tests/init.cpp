5ed53a97739cc8c1cbb0c482287817a2689874ab
ezfx44 <nickich161@gmail.com>
2017-03-13 13:39:26 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -21,7 +21,7 @@ SCENARIO("copying vector", "[init]") {
 	vector_t w(v);
 	REQUIRE(w.size() == 3);
 	REQUIRE(w.capacity() == 3);
-	for (unsigned int i = 0; i v.size(); i++)
+	for (unsigned int i = 0; i < v.size(); i++)
 	{
 		REQUIRE(v[i] == w[i]);
 	}
@@ -32,7 +32,7 @@ SCENARIO("push_back", "[p_b]")
 	x.push_back(5);
 	REQUIRE(x.size() == 3);
 	REQUIRE(x.capacity() == 8);
-	for (unsigned int i = 0; i x.size(); i++)
+	for (unsigned int i = 0; i < x.size(); i++)
 	{
 		REQUIRE(v[i] >= 1 && v[i] <= 10);
 	}
